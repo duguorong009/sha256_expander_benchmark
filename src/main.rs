0138f3f75de31040532f5a4c3df8cec9447ce65c
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2025-04-23 20:05:14 +0800

    feat: add simple sha256 circuit workflow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+use circuit_std_rs::sha256::gf2::SHA256GF2;
+use expander_compiler::frontend::*;
+use rand::RngCore;
+use sha2::{Digest, Sha256};
+
+// ref: https://github.com/PolyhedraZK/ExpanderCompilerCollection/blob/master/circuit-std-rs/tests/sha256_gf2.rs#L89-L137
+const INPUT_LEN: usize = 11 * 8; // input size in bits, must be a multiple of 8
+const OUTPUT_LEN: usize = 256; // FIXED 256
+
+declare_circuit!(SHA256Circuit {
+    input: [Variable; INPUT_LEN],
+    output: [Variable; OUTPUT_LEN],
+});
+
+impl Define<GF2Config> for SHA256Circuit<Variable> {
+    fn define<Builder: RootAPI<GF2Config>>(&self, api: &mut Builder) {
+        let mut hasher = SHA256GF2::new();
+        hasher.update(&self.input);
+        let output = hasher.finalize(api);
+        (0..OUTPUT_LEN).for_each(|i| api.assert_is_equal(output[i], self.output[i]));
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    assert!(INPUT_LEN % 8 == 0);
+    // let compile_result =
+    //     compile_generic(&SHA256Circuit::default(), CompileOptions::default()).unwrap();
+
+    let compile_result =
+        compile_cross_layer(&SHA256Circuit::default(), CompileOptions::default()).unwrap();
+
+    let mut rng = rand::rng();
+    let data = [rng.next_u32() as u8; INPUT_LEN / 8];
+    let mut hash = Sha256::new();
+    hash.update(data);
+    let output = hash.finalize();
+    let mut assignment = SHA256Circuit::default();
+    for i in 0..INPUT_LEN / 8 {
+        for j in 0..8 {
+            assignment.input[i * 8 + j] = (((data[i] >> (7 - j)) & 1) as u32).into();
+        }
+    }
+    for i in 0..OUTPUT_LEN / 8 {
+        for j in 0..8 {
+            assignment.output[i * 8 + j] = (((output[i] >> (7 - j) as u32) & 1) as u32).into();
+        }
+    }
+    let witness = compile_result
+        .witness_solver
+        .solve_witness(&assignment)
+        .unwrap();
+    let output = compile_result.layered_circuit.run(&witness);
+    assert_eq!(output, vec![true]);
 }
