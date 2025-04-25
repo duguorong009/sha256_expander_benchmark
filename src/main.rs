b2d64c38b151cc1ef112eba042f53cefc8edb9a9
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2025-04-25 23:43:43 +0800

    feat: impl logic of proving the cross layer prove gkr

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use circuit_std_rs::sha256::gf2::SHA256GF2;
 use expander_compiler::frontend::*;
+use expander_transcript::BytesHashTranscript;
+use gkr_engine::Transcript;
+use gkr_hashers::SHA256hasher;
 use rand::RngCore;
-use serdes::ExpSerde;
 use sha2::{Digest, Sha256};
 
 // ref: https://github.com/PolyhedraZK/ExpanderCompilerCollection/blob/master/circuit-std-rs/tests/sha256_gf2.rs#L89-L137
-const INPUT_LEN: usize = 10 * 8; // input size in bits, must be a multiple of 8
+const INPUT_LEN: usize = 100 * 8; // input size in bits, must be a multiple of 8
 const OUTPUT_LEN: usize = 256; // FIXED 256
 
 declare_circuit!(SHA256Circuit {
     input: [Variable; INPUT_LEN],
-    output: [PublicVariable; OUTPUT_LEN],
+    output: [Variable; OUTPUT_LEN],
 });
 
 impl Define<GF2Config> for SHA256Circuit<Variable> {
@@ -59,16 +61,37 @@ fn main() {
     let output = compile_result.layered_circuit.run(&witness);
     assert_eq!(output, vec![true]);
 
-    // create "circuit.txt"
-    let file = std::fs::File::create("build/circuit.txt").unwrap();
-    let writer = std::io::BufWriter::new(file);
-    compile_result
-        .layered_circuit
-        .serialize_into(writer)
-        .unwrap();
+    let expander_circuit = compile_result.layered_circuit
+        .export_to_expander::<gkr_engine::GF2ExtConfig>()
+        .flatten();
 
-    // create "witness.txt"
-    let file = std::fs::File::create("build/witness.txt").unwrap();
-    let writer = std::io::BufWriter::new(file);
-    witness.serialize_into(writer).unwrap();
+    let (simd_input, simd_public_input) = witness.to_simd::<gf2::GF2x8>();
+    println!("{} {}", simd_input.len(), simd_public_input.len());
+    assert_eq!(simd_public_input.len(), 0); // public input is not supported in current virgo++
+
+    let mut transcript = BytesHashTranscript::<
+        <gkr_engine::GF2ExtConfig as gkr_engine::FieldEngine>::ChallengeField,
+        SHA256hasher,
+    >::new();
+    
+    let connections = crosslayer_prototype::CrossLayerConnections::parse_circuit(&expander_circuit);
+    
+    let start_time = std::time::Instant::now();
+    let evals = expander_circuit.evaluate(&simd_input);
+    let mut sp = crosslayer_prototype::CrossLayerProverScratchPad::<gkr_engine::GF2ExtConfig>::new(
+        expander_circuit.layers.len(),
+        expander_circuit.max_num_input_var(),
+        expander_circuit.max_num_output_var(),
+        1,
+    );
+    let (_output_claim, _input_challenge, _input_claim) = crosslayer_prototype::prove_gkr(
+        &expander_circuit,
+        &evals,
+        &connections,
+        &mut transcript,
+        &mut sp,
+    );
+    let stop_time = std::time::Instant::now();
+    let duration = stop_time.duration_since(start_time);
+    println!("Time elapsed {} ms for proving", duration.as_millis());
 }
