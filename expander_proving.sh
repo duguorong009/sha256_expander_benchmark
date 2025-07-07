94fb9460e84b7844a6d2e2e66b93f54a32ca0641
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2025-07-07 15:26:36 +0800

    exp: upgrade 'ecc' and 'expander' version as 'v1.2.0'

--- expander_proving.sh
+++ expander_proving.sh
@@ -52,7 +52,7 @@ if [ ! -d "$EXPANDER_DIR" ]; then
   echo "Step 2: Cloning the Expander repository..."
   git clone $EXPANDER_REPO
   cd $EXPANDER_DIR
-  git checkout 827fb08
+  git checkout v1.2.0
   cargo run --bin=dev-setup --release
   cd -
 fi
